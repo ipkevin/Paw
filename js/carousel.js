bd65b964bd27a3b7958ce7191711069b65392a14
ipkevin <ipkevin@gmail.com>
2023-06-08 17:20:10 -0700

    adjust carousel settings to be smoother scrolling (note that responsive sets are additive so if u don't want a value applying to a set, you must overwrite it) mainly thru TouchThreshold. Also removed dots per Josh reco

--- js/carousel.js
+++ js/carousel.js
@@ -1,23 +1,25 @@
 $('.activities__carousel').slick({
   arrows: false,
-  dots: true,
+  dots: false,
   centerMode: false,
   infinite: true,
   centerPadding: '140px',
   slidesToShow: 3, // increase on larger screens to ensure start with items all across
+  slidesToScroll: 3,
   variableWidth: true,
   swipeToSlide: true,
-  // responsive: [
-  //   {
-  //     breakpoint: 768,
-  //     settings: {
-  //       arrows: false,
-  //       centerMode: true,
-  //       centerPadding: '140px',
-  //       infinite: true,
-  //       slidesToShow: 1,
-  //       variableWidth: false,
-  //     }
-  //   }
-  // ]
+  touchThreshold: 60,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '140px',
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      }
+    }
+  ]
 });
