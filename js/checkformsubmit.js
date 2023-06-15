82a8a438a9af9090c239c902243e41ef653c761e
ipkevin <ipkevin@gmail.com>
2023-06-15 10:06:21 -0700

    replace and style logo over form so that it has gap btwn it and form border, test new feature images

--- js/checkformsubmit.js
+++ js/checkformsubmit.js
@@ -2,13 +2,11 @@
 window.addEventListener('message', function(event) {
     // Check the origin of the message for security
     if (event.origin !== "https://r2.dotdigital-pages.com") {
-        console.log("did not pass origin check");
         return;
     }
     
     // Access the message data sent from the iframe
     var messageData = event.data;
-    console.log('Received message from iframe!:', messageData);
     if (messageData === "upper form submitted") {
         dataLayer.push({'event': 'upper-form-submitted'});
     } else if (messageData == "lower form submitted") {
